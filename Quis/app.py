1b7b12b97c4607fb644390bdc19e29831822e8c5
Fikriyah Khairunnisa <75004815+fikriyah64@users.noreply.github.com>
2022-04-01 10:28:52 +0700

    Add files via upload

---
+++ Quis/app.py
@@ -0,0 +1,27 @@
+#Fikriyah Khairunnisa-19090070-6A
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/api/bmi', methods=['POST'])
+def bmi():
+    height = float(request.form.get('height'))
+    weight = float(request.form.get('weight'))
+    bmi = weight / (height/100)**2
+    msg = "BMI anda " + str(bmi)
+    if bmi <= 18.5 :
+        ket = " kurus "
+    elif bmi <= 25 :
+        ket = " normal"
+    elif bmi <= 40 :
+        ket = "obesitas"
+    else :
+        ket = "terlalu"
+    data = {'result': 'true', 'msg': msg, 'ket':ket}
+    return jsonify(data)
+
+
+
+if __name__ == "__main__":
+    app.run(port=1300, debug=True) 
+   
